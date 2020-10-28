3e6b88b991ad0ff859ec269dba365efbf2bbe9a8
davidmarin25 <davidmarinospina@gmail.com>
2020-10-27 19:09:27 -0500

    Update EjerciciosT1.R

--- ejercicios/EjerciciosT1.R
+++ ejercicios/EjerciciosT1.R
@@ -1,11 +1,11 @@
 opBasic = function(a,b){
   print("Suma")
-  print(a+b)
+  print(paste(sprintf("%i + %i = ",a,b),a+b))
   print("Resta")
   print(paste(sprintf("%i - %i = ",a,b),a-b))
   print(paste(sprintf("%i - %i = ",b,a),b-a))
   print("Producto")
-  print(a*b)
+  print(paste(sprintf("%i * %i = ",a,b),a*b))
   print("Cociente de la división entera")
   print(paste(sprintf("%i : %i = ",a,b),a%/%b))
   print(paste("con resto ",a%%b))
